95cd039dbed3c256323ba5493b5ff7ccc152c75d
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-05-27 12:50:17 +1200

    Create print.py

---
+++ print.py
@@ -0,0 +1,20 @@
+# Print information about your self
+print("""Name: Arishay 
+  Address: 21 Henderson Valley 
+  Phone: 123456""")
+print("Name: Arishay \nAddress: 21 Henderson Valley \nPhone: 123456")
+
+# Print numbers to see what happens, does it actually work
+print (3)
+print(7 + 3)
+print(10 / 2)
+print(9 * 9)
+
+# Print your name with, input function 
+print("Enter your name:")
+x = input()
+print("Hello, " + x)
+
+# Print your name input alternate way
+name = input("What is your name? ")
+print("Hello, {}".format(name))
