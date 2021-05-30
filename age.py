6bf81f2907a8a74b5a66ec7d02103df11416a3b9
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-05-31 10:03:44 +1200

    age.py

---
+++ age.py
@@ -0,0 +1,8 @@
+# Input of users age
+age = int(input("How old are you? ")
+
+# Calculators age of user in 10 years
+age_10 = age + 10
+
+# How old user will be in 10 years
+print(" In 10 years you will be .format(age_10))
