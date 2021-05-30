2ce7db21e9233383907f0139522e124322f42ae9
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-05-31 09:52:29 +1200

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,13 @@
+#Glazed donuts cost $3, filled donuts $4 and mini donuts
+
+#Calculator total cost for order #1: 5 glazed, 3 filled and 6 mini donuts
+order1_cost = 5 * 3 + 3 + 3 * 4 + 6 *2
+
+
+#Calculate total cost for order #2: 2 glazed, 1 filled and 10 mini donuts
+order2_cost = 2 * 3 + 4 + 10 *2
+
+
+#Order print
+print("Your order 1 is: ${}".format(order1_cost))
+print("Your order 2 is: ${}".format(order2_cost))
