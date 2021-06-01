ecf253f99d6f1ba4993abd509ec7a29c55486b2c
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-02 11:58:12 +1200

    Create selection_age.py

---
+++ selection_age.py
@@ -0,0 +1,10 @@
+my_age = 16
+your_age = int(input("What is your age? " ))
+if your_age == 16:
+  print("We're the same age") 
+elif your_age > 20 :
+  print("You've finished High School a few years ago")
+elif your_age < 11:
+  print("You are less than 11 years old")
+else: 
+  print("I'm {}".format(my_age))
