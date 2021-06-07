58e4f4c5ecbb8fc1b297fbf528cde40eb2549044
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-08 10:24:44 +1200

    Create condtions.py

---
+++ condtions.py
@@ -0,0 +1,12 @@
+print("Hello")
+
+ans = input("Who is the current PM of NZ?  \n A. Jacinda Ardern \n B. Judith Collins ")
+
+if ans.lower() == "A":
+  print("Correct!")
+if ans.lower() == "B":
+  print("Incorrect")
+else: 
+  print("Incorrect")
+
+print("Thanks!")
