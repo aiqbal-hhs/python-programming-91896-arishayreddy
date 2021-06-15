e39d137e15885085d472023007c00e2b6e1b6f39
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-15 18:28:37 +1200

    Create coin_flipper.py

---
+++ coin_flipper.py
@@ -0,0 +1,10 @@
+def on_button_pressed_a():
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    basic.show_icon(IconNames.DIAMOND)
+    basic.show_icon(IconNames.SMALL_DIAMOND)
+    if Math.random_boolean():
+        basic.show_icon(IconNames.SKULL)
+    else:
+        basic.show_icon(IconNames.SQUARE)
+input.on_button_pressed(Button.A, on_button_pressed_a)
