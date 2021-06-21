98ac3ca23804fdd105ef4c31c3344616bf8018f9
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-22 10:57:20 +1200

    Create boundary.py

---
+++ boundary.py
@@ -0,0 +1,8 @@
+if 5 < age < 18 or age <= 80:
+  print("You will get a dsicount")
+  if age < 5:
+    print("No discount")
+  if age > 5 or age = 18:
+    print("Yes discount")
+  if age > 18:
+    print("No discount")
