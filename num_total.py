5a8f209e5fc7085a00b45d6c192ab6a8e558ee31
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-24 12:51:46 +1200

    Create num_total.py

---
+++ num_total.py
@@ -0,0 +1,11 @@
+# Ask user what their first number is
+num_1 = int(input("What is your first number? "))
+
+# Ask user what thier second number is
+num_2 = int(input("What is your second number? "))
+
+# This when you add two numbers together 
+num_total = num_1 + num_2
+
+# Print the numbers the user used and give answer
+print("{} + {} = {}".format(num_1, num_2, num_total))
