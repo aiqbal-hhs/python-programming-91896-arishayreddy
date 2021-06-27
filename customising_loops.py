b613717952c43f9996fd95255d6ba7c58d54c66b
Arishay Reddy <64936362+arishayreddy@users.noreply.github.com>
2021-06-28 09:51:38 +1200

    Create customising_loops.py

---
+++ customising_loops.py
@@ -0,0 +1,13 @@
+# Print numbers 0 -  8
+for i in range (9):
+  print(i)
+
+# Print I will practice coding everyday  5 times
+for i in range (5):
+  print("I will practice coding everyday")
+
+# Print Hip hip hooray 3 times
+for i in range (3):
+  print("Hip hip...")
+  print("Hooray!")
+  
